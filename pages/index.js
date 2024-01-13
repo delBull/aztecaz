18d8082597e2dcf01e4d77de444e45f3128694f4
Marco Del Toro <Marco@delBull-2.local>
2024-01-13 16:01:02 -0600

    Update typos

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ export default function Home() {
                                 <h1 data-wow-delay="0s" className="wow fadeInUp" style={{ lineHeight: '1.1' }}>Invierte en el futuro de la Tokenización Inmobiliaria</h1>
                                         <p data-wow-delay="0.1s" className="wow fadeInUp" style={{ fontSize: '26px', lineHeight: '1.1' }}>Aplicacamos herramientas blockchain para impulsar el crecimiento en el sector inmobiliario.</p>
                                 <div data-wow-delay="0.2s" className="wow fadeInUp flat-button flex">
-                                    <Link href="/coming-soon" className="tf-button style-1 h50 w190 mr-30">¡Empieza Ahora!<i className="icon-arrow-up-right2" /></Link>
+                                    <Link href="/market-wallet" className="tf-button style-1 h50 w190 mr-30">¡Empieza Ahora!<i className="icon-arrow-up-right2" /></Link>
                                 </div>
                             </div>
                         </div>
@@ -533,7 +533,7 @@ export default function Home() {
             </div>
             <div className="row">
                                 <div className="col-12">
-                                    <Link href="/coming-soon" className="tf-button style-1 h50 w190 m-auto">Empieza ya!<i className="icon-arrow-up-right2" /></Link>
+                                    <Link href="/market-wallet" className="tf-button style-1 h50 w190 m-auto">Empieza ya!<i className="icon-arrow-up-right2" /></Link>
                                 </div>
                             </div>
             <div style={{ padding: '50px' }}></div>
