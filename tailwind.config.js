df1d0f714d493fb67fa6259c492add6e6a0ddb1f
delBull <marco.munoz9@gmail.com>
2024-11-20 14:27:01 -0600

    Proyecto limpio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",   // Rutas para las páginas
+    "./components/**/*.{js,ts,jsx,tsx}",  // Rutas para los componentes
+    "./app/**/*.{js,ts,jsx,tsx}",        // Rutas para los archivos app (si usas esta carpeta)
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
