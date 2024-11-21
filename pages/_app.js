ce39a1a54b3233d781cb365899beccf9994427fa
delBull <marco.munoz9@gmail.com>
2024-11-21 01:13:13 -0600

    css fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "swiper/css/navigation"
 import "swiper/css/pagination"
 import 'swiper/css/free-mode'
 import "/public/assets/css/style.css"
-import "/public/assets/css/responsive.css"
 
 function MyApp({ Component, pageProps }) {
     const [loading, setLoading] = useState(false)
